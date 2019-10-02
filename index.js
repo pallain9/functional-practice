66a8db1ec42482b9549198bb4c645db9e056d871
Peter allain <pballain910@gmail.com>
2019-10-02 15:55:47 -0400

    passing 3/4 tests

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function calculateTimeSpent(chores) {
-  return chores
+function changeTaskOwner(chore) {
+
 }
 
+
+
+function totalTimeSpent(chore) {
+  const timeSpent = chore.minutes * chore.timesDone
+  chore.timeSpent = parseInt(timeSpent * 100) / 100
+
+  return chore
+}
+
+function calculateTimeSpent(chores) {
+  return chores.map(totalTimeSpent)
+}
+
+function environmentFilter(log) {
+  return log.environment === 'qa'
+}
+
+
 function filterForQA(logs) {
-  return logs
+  return logs.filter(environmentFilter)
 }
 
 function filterByName(chores, name) {
-  return chores
+  return chores.filter((chore) => {
+    return chore.doneBy === 'Jamal'
+  })
 }
 
 function assignTasks(tasks, from, to) {
